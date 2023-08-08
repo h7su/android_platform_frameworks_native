01c474cbc45458a56da0d7b3b01e1f93b439c500
Andrei Homescu <ahomescu@google.com>
2023-08-08 02:52:23 +0000

    rpcbinder: Add Trusty support
    Add a Rust wrapper over RpcServer to allow
    writing RPC Binder services in Rust.
    
    Bug: 242243245
    Test: build Trusty
    Change-Id: I55476c203c97f537a9f691cfafe72849e89a349b

--- libs/binder/rust/rpcbinder/src/server.rs
+++ libs/binder/rust/rpcbinder/src/server.rs
@@ -18,8 +18,11 @@ use crate::session::FileDescriptorTransportMode;
 use binder::{unstable_api::AsNative, SpIBinder};
 use binder_rpc_unstable_bindgen::ARpcServer;
 use foreign_types::{foreign_type, ForeignType, ForeignTypeRef};
-use std::ffi::CString;
 use std::io::{Error, ErrorKind};
+
+#[cfg(not(target_os = "trusty"))]
+use std::ffi::CString;
+#[cfg(not(target_os = "trusty"))]
 use std::os::unix::io::{IntoRawFd, OwnedFd};
 
 foreign_type! {
@@ -38,6 +41,7 @@ unsafe impl Send for RpcServer {}
 /// SAFETY: The underlying C++ RpcServer class is thread-safe.
 unsafe impl Sync for RpcServer {}
 
+#[cfg(not(target_os = "trusty"))]
 impl RpcServer {
     /// Creates a binder RPC server, serving the supplied binder service implementation on the given
     /// vsock port. Only connections from the given CID are accepted.
@@ -119,7 +123,22 @@ impl RpcServer {
             ))
         }
     }
+}
 
+#[cfg(target_os = "trusty")]
+impl RpcServer {
+    /// Creates a binder RPC server that can be added to a tipc Dispatcher.
+    pub fn new_trusty(mut service: SpIBinder) -> Result<RpcServer, Error> {
+        let service = service.as_native_mut();
+
+        // SAFETY: Takes ownership of the returned handle, which has correct refcount.
+        unsafe {
+            Self::checked_from_ptr(binder_rpc_unstable_bindgen::ARpcServer_newTrusty(service))
+        }
+    }
+}
+
+impl RpcServer {
     unsafe fn checked_from_ptr(ptr: *mut ARpcServer) -> Result<RpcServer, Error> {
         if ptr.is_null() {
             return Err(Error::new(ErrorKind::Other, "Failed to start server"));
@@ -146,7 +165,10 @@ impl RpcServerRef {
             )
         }
     }
+}
 
+#[cfg(not(target_os = "trusty"))]
+impl RpcServerRef {
     /// Starts a new background thread and calls join(). Returns immediately.
     pub fn start(&self) {
         // SAFETY: RpcServerRef wraps a valid pointer to an ARpcServer.
@@ -169,5 +191,78 @@ impl RpcServerRef {
         } else {
             Err(Error::from(ErrorKind::UnexpectedEof))
         }
+    }
+}
+
+#[cfg(target_os = "trusty")]
+pub struct RpcServerConnection {
+    ctx: *mut std::ffi::c_void,
+}
+
+#[cfg(target_os = "trusty")]
+impl Drop for RpcServerConnection {
+    fn drop(&mut self) {
+        // SAFETY: handleTipcChannelCleanup is the destructor for TipcChannelContext.
+        // ARpcServer_handleTipcConnect stores a pointer to a newly allocated
+        // TipcChannelContext on success in ctx.
+        // We do not need to close handle_fd since we do not own it.
+        unsafe {
+            binder_rpc_unstable_bindgen::ARpcServer_handleTipcChannelCleanup(self.ctx);
+        }
+    }
+}
+
+#[cfg(target_os = "trusty")]
+impl tipc::UnbufferedService for RpcServer {
+    type Connection = RpcServerConnection;
+
+    fn on_connect(
+        &self,
+        _port: &tipc::PortCfg,
+        handle: &tipc::Handle,
+        peer: &tipc::Uuid,
+    ) -> tipc::Result<tipc::ConnectResult<Self::Connection>> {
+        let mut conn = RpcServerConnection { ctx: std::ptr::null_mut() };
+        // SAFETY: self.as_ptr() should be a pointer to a valid ARpcServer.
+        // conn.ctx is a raw pointer that the callee will store the valid address
+        // of a newly allocated TipcChannelContext upon success. It is our
+        // responsiblity to call ARpcServer_handleTipcChannelCleanup later
+        // to clean it up.
+        let rc = unsafe {
+            binder_rpc_unstable_bindgen::ARpcServer_handleTipcConnect(
+                self.as_ptr(),
+                handle.as_raw_fd(),
+                peer.as_ptr().cast(),
+                &mut conn.ctx,
+            )
+        };
+        if rc < 0 {
+            Err(tipc::TipcError::from_uapi(rc.into()))
+        } else {
+            Ok(tipc::ConnectResult::Accept(conn))
+        }
+    }
+
+    fn on_message(
+        &self,
+        conn: &Self::Connection,
+        _handle: &tipc::Handle,
+        buffer: &mut [u8],
+    ) -> tipc::Result<tipc::MessageResult> {
+        assert!(buffer.is_empty());
+        // SAFETY: We should only be calling this with a valid ctx
+        // returned by ARpcServer_handleTipcConnect.
+        let rc = unsafe { binder_rpc_unstable_bindgen::ARpcServer_handleTipcMessage(conn.ctx) };
+        if rc < 0 {
+            Err(tipc::TipcError::from_uapi(rc.into()))
+        } else {
+            Ok(tipc::MessageResult::MaintainConnection)
+        }
+    }
+
+    fn on_disconnect(&self, conn: &Self::Connection) {
+        // SAFETY: We should only be calling this with a valid ctx
+        // returned by ARpcServer_handleTipcConnect.
+        unsafe { binder_rpc_unstable_bindgen::ARpcServer_handleTipcDisconnect(conn.ctx) };
     }
 }
