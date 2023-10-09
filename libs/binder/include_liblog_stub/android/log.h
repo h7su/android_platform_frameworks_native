3f77fbce27f4edc972ae5c610cacc9c800c8a9fd
Steven Moreland <smoreland@google.com>
2023-10-09 22:22:27 +0000

    Introducing libbinder_sdk
    libbinder_sdk is binder with all of its dependencies pulled in, so that
    it can be used on other platforms.
    
    Bug: 285204695
    Test: mma
    Change-Id: I32197cc2464562489c63fc5d0e08157deb26235e

---
+++ libs/binder/include_liblog_stub/android/log.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*__android_logger_function)(const struct __android_log_message* log_message);
+inline void __android_log_set_logger(__android_logger_function) {}
+inline void __android_log_stderr_logger(const struct __android_log_message*) {}
+
+#ifdef __cplusplus
+}
+#endif
