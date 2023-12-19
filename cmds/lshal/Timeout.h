24f58d61fa2890f49230bb7b4e3392f6c627284f
Yifan Hong <elsk@google.com>
2023-12-18 17:39:20 -0800

    lshal: use std::async
    All of the commands are executed by starting a thread,
    and if there is a timeout, sending a SIGINT to the thread,
    which invokes pthread_exit from the signal handler. If
    pthread_exit is called while the thread is in jemalloc code,
    that might cause problems.
    
    Use a standard library std::async function to avoid handling
    pthread's manually. This avoids calling pthread_kill(). If
    the function times out, simply ignore the future object and
    move on. Although this causes memory leak, the lshal tool is
    a debugging tool that is intended to run for a short period of
    time, not as a daemon. So this is okay.
    
    Test: lshal_test
    Bug: 311143089
    Change-Id: Id1092fbc3992c62c13c66ddac18105e8dcd6fc81

--- cmds/lshal/Timeout.h
+++ cmds/lshal/Timeout.h
@@ -16,92 +16,50 @@
 
 #pragma once
 
-#include <condition_variable>
 #include <chrono>
-#include <functional>
-#include <mutex>
-#include <thread>
+#include <future>
+#include "utils/Errors.h"
 
 #include <hidl/Status.h>
 
 namespace android {
 namespace lshal {
 
-static constexpr std::chrono::milliseconds IPC_CALL_WAIT{500};
+constexpr std::chrono::milliseconds IPC_CALL_WAIT{500};
 
-class BackgroundTaskState {
-public:
-    explicit BackgroundTaskState(std::function<void(void)> &&func)
-            : mFunc(std::forward<decltype(func)>(func)) {}
-    void notify() {
-        std::unique_lock<std::mutex> lock(mMutex);
-        mFinished = true;
-        lock.unlock();
-        mCondVar.notify_all();
-    }
-    template<class C, class D>
-    bool wait(std::chrono::time_point<C, D> end) {
-        std::unique_lock<std::mutex> lock(mMutex);
-        mCondVar.wait_until(lock, end, [this](){ return this->mFinished; });
-        return mFinished;
-    }
-    void operator()() {
-        mFunc();
-    }
-private:
-    std::mutex mMutex;
-    std::condition_variable mCondVar;
-    bool mFinished = false;
-    std::function<void(void)> mFunc;
-};
-
-void *callAndNotify(void *data) {
-    BackgroundTaskState &state = *static_cast<BackgroundTaskState *>(data);
-    state();
-    state.notify();
-    return nullptr;
-}
-
-template<class R, class P>
-bool timeout(std::chrono::duration<R, P> delay, std::function<void(void)> &&func) {
-    auto now = std::chrono::system_clock::now();
-    BackgroundTaskState state{std::forward<decltype(func)>(func)};
-    pthread_t thread;
-    if (pthread_create(&thread, nullptr, callAndNotify, &state)) {
-        std::cerr << "FATAL: could not create background thread." << std::endl;
-        return false;
-    }
-    bool success = state.wait(now + delay);
-    if (!success) {
-        pthread_kill(thread, SIGINT);
-    }
-    pthread_join(thread, nullptr);
-    return success;
-}
-
+// Call function on interfaceObject and wait for result until the given timeout has reached.
+// Callback functions pass to timeoutIPC() may executed after the this function
+// has returned, especially if deadline has been reached. Hence, care must be taken when passing
+// data between the background thread and the main thread. See b/311143089.
 template<class R, class P, class Function, class I, class... Args>
 typename std::result_of<Function(I *, Args...)>::type
 timeoutIPC(std::chrono::duration<R, P> wait, const sp<I> &interfaceObject, Function &&func,
            Args &&... args) {
     using ::android::hardware::Status;
-    typename std::result_of<Function(I *, Args...)>::type ret{Status::ok()};
-    auto boundFunc = std::bind(std::forward<Function>(func),
-            interfaceObject.get(), std::forward<Args>(args)...);
-    bool success = timeout(wait, [&ret, &boundFunc] {
-        ret = std::move(boundFunc());
-    });
-    if (!success) {
+
+    // Execute on a background thread but do not defer execution.
+    auto future =
+            std::async(std::launch::async, func, interfaceObject, std::forward<Args>(args)...);
+    auto status = future.wait_for(wait);
+    if (status == std::future_status::timeout) {
         return Status::fromStatusT(TIMED_OUT);
     }
-    return ret;
+    if (status != std::future_status::ready) {
+        return Status::fromExceptionCode(Status::Exception::EX_ILLEGAL_STATE,
+                                         "Illegal future_status");
+    }
+    return future.get();
 }
 
+// Call function on interfaceObject and wait for result until the default timeout has reached.
+// Callback functions pass to timeoutIPC() may executed after the this function
+// has returned, especially if deadline has been reached. Hence, care must be taken when passing
+// data between the background thread and the main thread. See b/311143089.
 template<class Function, class I, class... Args>
 typename std::result_of<Function(I *, Args...)>::type
 timeoutIPC(const sp<I> &interfaceObject, Function &&func, Args &&... args) {
     return timeoutIPC(IPC_CALL_WAIT, interfaceObject, func, args...);
 }
 
-
-}  // namespace lshal
-}  // namespace android
+} // namespace lshal
+} // namespace android
