littleGnAl/mirror_platform_frameworks_native

DO NOT SUBMIT: binder: pass interface descriptor with binder
TODO: measure memory impact

When sending any binder around, this also sends
the type information. The main benefit of this
is that, you can always check the type of a binder,
even if you aren't able to make a binder call, or
if the binder is dead. Specifically, when binder
proxies leak in Java, this would make sure we're
still able to print the type of the binder.

This adds memory use and reduces transactions, but
it also opens up a variety of other possibilities
or future work:
- we could switch RPC binder to using this same
  optimization and remove uses of locks from
  binder proxies
- we could make interface_cast or Java Stub
  constructors check the type, so we would
  get type errors early
- we could switch writeInterfaceToken to avoid
  writing interface tokens (interface tokens would
  only be written the first time a binder is sent)
- we could move atrace logic from AIDL to libbinder
  so a single atrace call could catch all
  transactions (though, this would also need a way
  to convert code into method name, it would
  remove a huge amount of auto-generated code)
- we could cache descriptors, so that we didn't
  need to store copies of them, when multiple
  binder objects have the same type

Bug: N/A
Test: boot device, binderAllocationLimits
Change-Id: I4849d70b2f5632a89ff7f5d2a8afdd13730e50df

libs/binder/include/binder/BpBinder.h
@@ -130,7 +130,7 @@ public:
         friend class ::android::ProcessState;
         friend class ::android::RpcSession;
         friend class ::android::RpcState;
-        explicit PrivateAccessor(const BpBinder* binder) : mBinder(binder) {}
+        explicit PrivateAccessor(BpBinder* binder) : mBinder(binder) {}
 
         static sp<BpBinder> create(int32_t handle) { return BpBinder::create(handle); }
         static sp<BpBinder> create(const sp<RpcSession>& session, uint64_t address) {
@@ -144,9 +144,14 @@ public:
         uint64_t rpcAddress() const { return mBinder->rpcAddress(); }
         const sp<RpcSession>& rpcSession() const { return mBinder->rpcSession(); }
 
-        const BpBinder* mBinder;
+        status_t setDescriptor(const String16& desc) { return mBinder->setDescriptor(desc); }
+
+        BpBinder* mBinder;
     };
-    const PrivateAccessor getPrivateAccessor() const { return PrivateAccessor(this); }
+    const PrivateAccessor getPrivateAccessor() const {
+        return PrivateAccessor(const_cast<BpBinder*>(this));
+    }
+    PrivateAccessor getPrivateAccessor() { return PrivateAccessor(this); }
 
 private:
     friend PrivateAccessor;
@@ -190,8 +195,9 @@ private:
 
             void                reportOneDeath(const Obituary& obit);
             bool                isDescriptorCached() const;
+            status_t setDescriptor(const String16&);
 
-    mutable Mutex               mLock;
+            mutable Mutex mLock;
             volatile int32_t    mAlive;
             volatile int32_t    mObitsSent;
             Vector<Obituary>*   mObituaries;

h7su/android_platform_frameworks_native

libs/binder/include/binder/BpBinder.h
@@ -132,7 +132,7 @@ public:
         friend class ::android::ProcessState;
         friend class ::android::RpcSession;
         friend class ::android::RpcState;
-        explicit PrivateAccessor(const BpBinder* binder) : mBinder(binder) {}
+        explicit PrivateAccessor(BpBinder* binder) : mBinder(binder) {}
 
         static sp<BpBinder> create(int32_t handle) { return BpBinder::create(handle); }
         static sp<BpBinder> create(const sp<RpcSession>& session, uint64_t address) {
@@ -146,9 +146,14 @@ public:
         uint64_t rpcAddress() const { return mBinder->rpcAddress(); }
         const sp<RpcSession>& rpcSession() const { return mBinder->rpcSession(); }
 
-        const BpBinder* mBinder;
+        status_t setDescriptor(const String16& desc) { return mBinder->setDescriptor(desc); }
+
+        BpBinder* mBinder;
     };
-    const PrivateAccessor getPrivateAccessor() const { return PrivateAccessor(this); }
+    const PrivateAccessor getPrivateAccessor() const {
+        return PrivateAccessor(const_cast<BpBinder*>(this));
+    }
+    PrivateAccessor getPrivateAccessor() { return PrivateAccessor(this); }
 
 private:
     friend PrivateAccessor;
@@ -192,8 +197,9 @@ private:
 
             void                reportOneDeath(const Obituary& obit);
             bool                isDescriptorCached() const;
+            status_t setDescriptor(const String16&);
 
-    mutable RpcMutex            mLock;
+            mutable RpcMutex mLock;
             volatile int32_t    mAlive;
             volatile int32_t    mObitsSent;
             Vector<Obituary>*   mObituaries;