167a82cb796104a3a12f770fdc3de456114bfb83
Andrei Homescu <ahomescu@google.com>
2024-03-22 22:38:08 +0000

    libbinder: Add build flag to disable shell commands
    Shell commands are not supported on Trusty at all.
    Add a new BINDER_DISABLE_SHELL_COMMANDS build option
    to disable them.
    
    Bug: 242243245
    Test: build Trusty
    Change-Id: I41e21c3b0b8dd88a3e0a9ad30e1d25bcb07c9915

--- libs/binder/ndk/ibinder.cpp
+++ libs/binder/ndk/ibinder.cpp
@@ -211,6 +211,10 @@ status_t ABBinder::onTransact(transaction_code_t code, const Parcel& data, Parce
         binder_status_t status = getClass()->onTransact(this, code, &in, &out);
         return PruneStatusT(status);
     } else if (code == SHELL_COMMAND_TRANSACTION && getClass()->handleShellCommand != nullptr) {
+#ifdef BINDER_DISABLE_SHELL_COMMANDS
+        ALOGE("Shell commands are disabled");
+        return STATUS_INVALID_OPERATION;
+#else  // BINDER_DISABLE_SHELL_COMMANDS
         int in = data.readFileDescriptor();
         int out = data.readFileDescriptor();
         int err = data.readFileDescriptor();
@@ -253,6 +257,7 @@ status_t ABBinder::onTransact(transaction_code_t code, const Parcel& data, Parce
             resultReceiver->send(status);
         }
         return status;
+#endif  // BINDER_DISABLE_SHELL_COMMANDS
     } else {
         return BBinder::onTransact(code, data, reply, flags);
     }
